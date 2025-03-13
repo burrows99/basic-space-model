65c13dba0ed55699d22cd62820c7374f063d74a4
burrows99 <rburrows@sciometrix.com>
2025-03-13 16:36:41 +0000

    Created basic-space-engine but earth, moon and torus texture not showing up

--- src/objects/Earth.js
+++ src/objects/Earth.js
@@ -3,7 +3,7 @@ import * as THREE from 'three';
 export const getEarth = (moon, scene) => {
     const earthTexture = new THREE.TextureLoader().load('Images/Large_World_Physical_Map.png');
     const normalTexture = new THREE.TextureLoader().load('Images/normal.jpg');
-    const earthMaterial = new THREE.MeshStandardMaterial({ map: earthTexture, normalMap: normalTexture });
+    const earthMaterial = new THREE.MeshBasicMaterial({ map: earthTexture, normalMap: normalTexture });
     const earth = new THREE.Mesh(new THREE.SphereGeometry(12, 50, 50), earthMaterial);
     scene.add(earth);
     earth.add(moon);
--- src/objects/Moon.js
+++ src/objects/Moon.js
@@ -5,7 +5,7 @@ export const getMoon = () => {
     const normalTexture = new THREE.TextureLoader().load('Images/normal.jpg');
     const moon = new THREE.Mesh(
         new THREE.SphereGeometry(3, 32, 32),
-        new THREE.MeshStandardMaterial({ map: moonTexture, normalMap: normalTexture })
+        new THREE.MeshBasicMaterial({ map: moonTexture, normalMap: normalTexture })
     );
     moon.position.set(0, 0, 20);
     return moon;
